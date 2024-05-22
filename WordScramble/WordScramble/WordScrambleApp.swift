fa0f40ad73c9831531752ae72c5bd0162efea27b
ricky <ricky.y.chang@gmail.com>
2024-05-22 14:56:56 -0700

    WordScramble - added WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by ricky on 2024-05-21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
