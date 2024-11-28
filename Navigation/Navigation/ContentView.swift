95965a6395f55efb8af533563e63711e68f832a6
ricky <ricky.y.chang@gmail.com>
2024-11-28 14:40:54 -0800

    Navigation - Initial checkin for Navigation

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by ricky on 2024-11-20.
+//
+
+import SwiftUI
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path:"SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
+    }
+}
+
+struct ContentView: View {
+    @State private var path = PathStore()
+    
+    var body: some View {
+        NavigationStack(path: $path.path) {
+            DetailView(number: 0, path: $path.path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path.path)
+                }
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by ricky on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
